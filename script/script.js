1b5b8985afa9af2e616cc3d57f95e182b76e8ce0
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2022-05-24 21:07:12 -0700

    Example 2 Update

---
+++ script/script.js
@@ -0,0 +1,8 @@
+const cpBg = document.getElementById("cpBg");
+const body = document.getElementsByTagName("body")[0];
+
+
+
+cpBg.addEventListener('change', function() {
+    body.style.backgroundColor = cpBg.value;
+})
